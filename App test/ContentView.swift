f5b3bdee8ae5435550ccce3e52466fd19000834a
yian6622 <yc6622@nyu.edu>
2024-02-12 14:42:50 -0500

    Initial Commit

---
+++ App test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App test
+//
+//  Created by 陈逸安 on 2/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
